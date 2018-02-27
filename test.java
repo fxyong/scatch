b7256262bd14225a4fc0f1370f239fa5e4055259
Jane Seah <jane.seah.2016@sis.smu.edu.sg>
2018-02-27 11:42:51 +0800

    Merged amanda's comments

--- test.java
+++ test.java
@@ -2,6 +2,8 @@ public class Test {
     public static void main(String[] args) {
         System.out.print("hello world");
         System.out.println("help");
+        //comments
+        //comments part 2
     }
 }
 
